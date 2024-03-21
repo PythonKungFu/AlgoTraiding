1b3fcb99b5c542539c568301e6c000b289579e5d
Dima <taurus700@yandex.ru>
2024-03-21 22:08:51 +0300

    Добавил в абстрактный класс Exchange константы: токен телеграм бота - оповещателя и свой id. Написал метод отправки сообщения в чат, где я и бот, для оповещений.

--- AlgoTrading/Exchanges/Exchange.py
+++ AlgoTrading/Exchanges/Exchange.py
@@ -1,11 +1,22 @@
 import requests
+from keys import *
+
 
 class Exchange:
     base_url = ''
-
+    TOKENTG = TOKENTELEGRAM
+    USERTG = USERTELEGRAM
 
     def any_request(self, method, params=None):
         return requests.get(self.base_url + method, params=params).json()
+
+    def send_message_telegram(self, message):
+        url = f"https://api.telegram.org/bot{self.TOKENTG}/sendMessage"
+        data = {
+            'chat_id': self.USERTG,
+            'text': message
+        }
+        return requests.post(url, data=data).json()
 
     def exchange_info(self):
         return self.any_request(method=None)
