a1595cba9539f3dba0f34ea2566c7a20eebe6f47
Dima <taurus700@yandex.ru>
2024-03-24 22:12:20 +0300

    Добавил в методы абстрактного класса Exchange type hinting и метод symbol_price который возвращает цену монеты

--- AlgoTrading/Exchanges/Exchange.py
+++ AlgoTrading/Exchanges/Exchange.py
@@ -7,11 +7,11 @@ class Exchange:
     TOKENTG = TOKENTELEGRAM
     USERTG = USERTELEGRAM
 
-    def any_request(self, method, params=None):
+    def any_request(self, method: str, params=None):
         """ Отправляем любой запрос с параметрами """
         return requests.get(self.base_url + method, params=params).json()
 
-    def send_message_telegram(self, message):
+    def send_message_telegram(self, message: str):
         """" Отправляем сообщение в телеграм"""
         url = f"https://api.telegram.org/bot{self.TOKENTG}/sendMessage"
         data = {
@@ -20,14 +20,18 @@ class Exchange:
         }
         return requests.post(url, data=data).json()
 
-    def exchange_info(self):
+    def exchange_info(self) -> dict:
         """" Получаем общую информацию по бирже """
         return self.any_request(method=None)
 
-    def get_trade_list(self, coin):
+    def get_trade_list(self, coin: str) -> list:
         """" Получаем список последних сделок """
         return self.any_request(method=None)
 
-    def ticker_24hr_price_change(self, symbol):
+    def ticker_24hr_price_change(self, symbol: str) -> dict:
         """" Получаем статистику изменения цен за 24 часа"""
+        pass
+
+    def get_symbol_price(self, symbol: str, rounding: int) -> float:
+        """" Получаем цену монеты """
         pass
